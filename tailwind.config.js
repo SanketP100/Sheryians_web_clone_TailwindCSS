ed74c7b789ac530c55eb97381092a0b9b56d8546
sanket <sankrtpowar@gmail.com>
2024-06-17 10:52:52 -0700

    Sheryians_web_clone_TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+
+      fontFamily: {
+        NeueMachinaLight:["NeueMachina-Light", 'sans-serif'],
+        NeueMachina:["NeueMachina-Regular", 'sans-serif'],
+        Juana:["Fontspring-DEMO-juana-extralightit", 'sans']
+      },
+
+    },
+  },
+  plugins: [],
+}
+
